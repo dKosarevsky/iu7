e50f0e21aff30dd1ff36b97aaa3b9004d563fc83
if <if>
2020-12-01 23:21:40 +0300

    os lab sockets

---
+++ 5sem/labs_os/sockets/sync_tcp_server.py
@@ -0,0 +1,24 @@
+# Синхронный tcp-сервер
+import socket
+
+
+# Задаем адрес сервера
+SERVER_ADDRESS = ('localhost', 8686)
+
+# Настраиваем сокет
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(SERVER_ADDRESS)
+server_socket.listen(10)
+print('server is running, please, press ctrl+c to stop')
+
+# Слушаем запросы
+while True:
+    connection, address = server_socket.accept()
+    print("new connection from {address}".format(address=address))
+
+    data = connection.recv(1024)
+    print(str(data))
+
+    connection.send(bytes('Hello from server!', encoding='UTF-8'))
+
+    connection.close()
